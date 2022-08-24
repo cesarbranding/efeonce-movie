3e4703ce1c37d4cd306f08dfd5a134e7e90e3c52
Julio Reyes <jreysgo@gmail.com>
2022-08-24 14:26:25 -0400

    Add category list

--- src/main.js
+++ src/main.js
@@ -21,3 +21,26 @@ async function getTrendingMoviesPreview() {
 }
 
 getTrendingMoviesPreview();
+
+async function getCategoriesPreview() {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`,
+    );
+    const data = await res.json();
+    const categories = data.genres;
+    categories.forEach(category => {
+      const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list'); // Crea un div contenedor de categorías de películas en el HTML
+      const categoryContainer = document.createElement('div'); // Crea un div en el HTML
+      categoryContainer.classList.add('category-container'); // Agrega la clase category-container al div
+  
+      const categoryTitle = document.createElement('h3'); // Crea un hr en el HTML para la categoría
+      categoryTitle.classList.add('category-title'); // Agrega la clase category-title al h3
+      categoryTitle.setAttribute('id', 'id' + category.id); // Agrega el atributo id al h3
+      const categoryTitleText = document.createTextNode(category.name) // Agrega el texto de la categoría al h3
+      categoryTitle.appendChild(categoryTitleText); // Agrega el h3 al div
+      categoryContainer.appendChild(categoryTitle); // Agrega el div al contenedor de categorías de películas
+      previewCategoriesContainer.appendChild(categoryContainer); // Agrega el div al contenedor de categorías de películas
+    });
+  }
+
+getCategoriesPreview();
